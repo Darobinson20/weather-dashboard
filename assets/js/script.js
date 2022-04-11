551f7746a26c2493e41117a2b087100290cf2ad3
Darobinson20 <Darobinson20@gmail.com>
2022-04-10 22:46:30 -0400

    Push 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 
-//API Key
-const APIKey = "5a2115b1ceabbd9cda28fba660eafa25"
-
-
-    
-fetch('https://api.openweathermap.org/data/2.5/weather?q=city&appid=5a2115b1ceabbd9cda28fba660eafa25')
-.then(res => res.json())
-.then(data => console.log(data))
-
 function initPage() {
     const cityEl = document.getElementById("enter-city");
     const searchEl = document.getElementById("city-name");
     const nameEl = document.getElementById("current-pic")
-    const currentTemp = document.getElementById("Temp:")
-     
+    const currentTempEl = document.getElementById("Temp:")
+    const currentwindEl = document.getElementById("Wind:")
+    const currenthumidityEl = document.getElementById("Humidity:")
+    const currentUVEl = document.getElementById("UV-Index:")
+    var todaysweatherEL = document.getElementById("todays-weather")
+    var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+}
 
+//API Key
+const APIKey = "5a2115b1ceabbd9cda28fba660eafa25";
+
+var getWeatherData = function(cityName) {
+
+    var apiUrl= "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+
+    console.log(something);
     
+   
 }
